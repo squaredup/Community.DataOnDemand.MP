f9f94e2a008a123ceffd144410559695192ddfa3
Matthew Long <matthew.long@squaredup.com>
2017-04-06 09:22:11 +0100

    GetNetstatCSV now handles elevation internally

--- ManagementPacks/Community.DataOnDemand.Unix/Scripts/GetNetstatCSV.sh
+++ ManagementPacks/Community.DataOnDemand.Unix/Scripts/GetNetstatCSV.sh
@@ -3,10 +3,13 @@
 # Copyright 2016 Squared Up Limited, All Rights Reserved.
 # Argument Block
 # Arg 1 = Format
+# Arg 2 = SCXUser configuration
 Format="$1"
 if [ -z "$Format" ]; then
     Format="csv"
 fi
+
+scxUser="$2"
 
 # Store hostname in case it's not availible in certain shells
 localHostName=$(hostname)
@@ -26,13 +29,18 @@ case "$Format" in
     ;;
 esac
 
-# Print Header, required by SQUP provider
-echo -n "Computername,PID,ProcessName,ProcessDescription,Protocol,LocalAddress,LocalPort,RemoteAddress,RemotePort,State,RemoteAddressIP$lineEnd"
+elevate=""
+if [ "$(id -u)" != "0" ]; then
+    # Not currently running as root, check if we are supposed to elevate or not    
+    case "$scxUser" in        
+        *\&lt;Elev\&gt;sudo\&lt;/Elev\&gt;*)
+            elevate="sudo"        
+        ;;
+    esac
+fi
 
-# Output netstat info in required format.  -tpn gives us TCP only connections, without host/port lookup, and includes PIDs
-netstat -tpn |
-    grep ESTABLISHED |    
-    awk -v ORS="$lineEnd" -v OFS=',' -v processDescMaxLength=$processDescMaxLength '{
+# Store AWK script 
+awkScript='{
         # Local Endpoint Address / Port split
         localEpSplit = match($4, ":[0-9]+$")
         localAddr = substr($4, 0, localEpSplit - 1)
@@ -45,27 +53,51 @@ netstat -tpn |
 
         # Extract PID
         split($7,pid, "/")        
-        
-        # Query for command (with args) that started the process
-        argQuery = "ps -o args= --pid " pid[1] " | cut -c-" processDescMaxLength
-        argQuery | getline args
-        close(argQuery)
 
-        # Append "..." if the string is max length and does not already end with those characters
-        if (length(args) == processDescMaxLength &amp;&amp; substr(args,length(args)-2,3) != "...")
-            args = args "..."
-        
-        # Escape double quotes, then Wrap the description in double quotes for CSV
-        gsub(/"/, "\"\"", args)
-        sub(/^[^"].+[^"]$/, "\"&amp;\"", args)
-        
-        # Query for the process name
-        commandQuery = "ps -o comm= --pid " pid[1]
-        commandQuery | getline comm
-        close(commandQuery)
+        if (pid[1] == "-" || pid[1] == "") 
+        {
+            comm = "Unknown"
+            args = ""
+            pid[1] = -1
+
+        }
+        else
+        {
+            # Query for command (with args) that started the process
+            argQuery = elevate "ps -o args= --pid " pid[1] " | cut -c-" processDescMaxLength
+            argQuery | getline args
+            close(argQuery)
+
+            # Append "..." if the string is max length and does not already end with those characters
+            if (length(args) == processDescMaxLength &amp;&amp; substr(args,length(args)-2,3) != "...")
+                args = args "..."
+            
+            # Escape double quotes, then Wrap the description in double quotes for CSV
+            gsub(/"/, "\"\"", args)
+            sub(/^[^"].+[^"]$/, "\"&amp;\"", args)
+            
+            # Query for the process name
+            commandQuery = elevate "ps -o comm= --pid " pid[1]
+            commandQuery | getline comm
+            close(commandQuery)
+        }
 
         # Finally, print records
         print "'"$localHostName"'", pid[1], comm, args, toupper($1), localAddr, localPort, remoteAddr, remotePort, $6, remoteAddr
-    }'
+}'
+
+# Print Header, required by SQUP provider
+echo -n "Computername,PID,ProcessName,ProcessDescription,Protocol,LocalAddress,LocalPort,RemoteAddress,RemotePort,State,RemoteAddressIP$lineEnd"
+
+# Output netstat info in required format.  -tpn gives us TCP only connections, without host/port lookup, and includes PIDs
+if [ "$elevate" != "sudo" ]; then
+    netstat -tpn |
+        grep ESTABLISHED |    
+            awk -v ORS="$lineEnd" -v OFS=',' -v processDescMaxLength=$processDescMaxLength -v elevate="" "$awkScript"
+else
+    sudo netstat -tpn |
+        grep ESTABLISHED |    
+            awk -v ORS="$lineEnd" -v OFS=',' -v processDescMaxLength=$processDescMaxLength -v elevate="sudo " "$awkScript"
+fi
 
 exit
